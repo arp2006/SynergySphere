b78f1ec346a34e8fa794dee34c2f62cc8c84f2ce
Adityaraj <adityarajpanda.arp@gmail.com>
2025-09-06 12:31:28 +0530

    final backend for auth

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ app.post("/register", async (req, res) => {
 
   try {
     const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [email]);
-    
+
     if (checkResult.rows.length > 0) {
-      return res.status(400).send("Email already exists. Try logging in.");
+      return res.json({ success: false, message: "Email already exists. Try logging in." });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -49,13 +49,38 @@ app.post("/register", async (req, res) => {
     );
 
     console.log("New user created");
-    res.status(201).send("User registered successfully.");
+    res.json({ success: true, message: "User registered successfully." });
   } catch (err) {
     console.error(err);
-    res.status(500).send("Internal server error");
+    res.json({ success: false, message: "Internal server error" });
   }
 });
 
+app.post("/login", async (req, res) => {
+  const email = req.body.em;
+  const password = req.body.pass;
+
+  try {
+    const userResult = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+
+    if (userResult.rows.length === 0) {
+      return res.json({ success: false, message: "Invalid email or password." });
+    }
+
+    const user = userResult.rows[0];
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.json({ success: false, message: "Invalid email or password." });
+    }
+    console.log("User logged in:", user.username);
+    res.json({ success: true, message: "Login successful.", username: user.username });
+  } 
+  catch (err) {
+    console.error(err);
+    res.json({ success: false, message: "Internal server error" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
